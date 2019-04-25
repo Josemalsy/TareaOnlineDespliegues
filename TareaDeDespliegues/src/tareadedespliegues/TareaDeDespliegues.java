ecccf4540f5ec333cc56dd192181b706e51e61b6
palomeque <antonioluispalomeque@gmail.com>
2019-04-25 16:44:20 +0200

    Versión 2

--- TareaDeDespliegues/src/tareadedespliegues/TareaDeDespliegues.java
+++ TareaDeDespliegues/src/tareadedespliegues/TareaDeDespliegues.java
@@ -5,6 +5,7 @@ public class TareaDeDespliegues {
     public static void main(String[] args) {
         
         System.out.println("Prueba para despliegues");
+        System.out.println("Esta siendo editado por Antonio Luis Palomeque López");
     }
 
 }
